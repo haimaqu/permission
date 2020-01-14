b946794b2fac10537eec566de119dadb154b886d
xf <854138497@qq.com>
2020-01-14 20:57:36 +0800

    6-7 Http请求前后监听工具

---
+++ src/main/java/com/mmall/common/HttpInterceptor.java
@@ -0,0 +1,63 @@
+package com.mmall.common;
+
+import com.mmall.util.JsonMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Slf4j
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+
+    /**
+     * 在请求处理前调用
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getRequestURI().toString();
+        Map parameterMap = request.getParameterMap();
+        log.info("request start. url:{},parameterMap:{}", url, JsonMapper.obj2String(parameterMap));
+
+        return true;
+    }
+
+    /**
+     * 正常处理请求结束后触发
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String url = request.getRequestURI().toString();
+        Map parameterMap = request.getParameterMap();
+        log.info("request finished. url:{},parameterMap:{}", url, JsonMapper.obj2String(parameterMap));
+
+    }
+
+    /**
+     * 任何情况下请求结束后
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String url = request.getRequestURI().toString();
+        Map parameterMap = request.getParameterMap();
+        log.info("request complete. url:{},parameterMap:{}", url, JsonMapper.obj2String(parameterMap));
+
+    }
+}
