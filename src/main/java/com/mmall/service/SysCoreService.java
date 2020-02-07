2595c0ff7750bf048fa7fda74cbdbe215a2821d3
xf <854138497@qq.com>
2020-02-07 19:32:49 +0800

    15-4 权限拦截功能开发-4

--- src/main/java/com/mmall/service/SysCoreService.java
+++ src/main/java/com/mmall/service/SysCoreService.java
@@ -6,6 +6,7 @@ import com.mmall.dao.SysAclMapper;
 import com.mmall.dao.SysRoleAclMapper;
 import com.mmall.dao.SysRoleUserMapper;
 import com.mmall.model.SysAcl;
+import com.mmall.model.SysUser;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
@@ -66,7 +67,13 @@ public class SysCoreService {
 
     // 是否是超级用户
     public boolean isSuperAdmin() {
-        return true;
+        // 这里是我自己定义了一个假的超级管理员规则，实际中要根据项目进行修改
+        // 可以是配置文件获取，可以指定某个用户，也可以指定某个角色
+        SysUser sysUser = RequestHolder.getCurrentUser();
+        if (sysUser.getMail().contains("admin")) {
+            return true;
+        }
+        return false;
     }
 
 
